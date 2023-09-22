85ad763f39d2d6a38ee57651e17ab2d620122975
Anderson Viana <andviana@hotmail.com>
2023-09-22 17:47:45 -0300

    problemas ao localizar lotes por tickets

--- src/Controller/LoteController.php
+++ src/Controller/LoteController.php
@@ -11,6 +11,7 @@ use App\Repository\LoteRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,23 +19,37 @@ use Symfony\Component\Routing\Annotation\Route;
 class LoteController extends AbstractController
 {
     #[Route('/', name: 'app_lote_index', methods: ['GET'])]
-    public function index(LoteRepository $loteRepository): Response
+    public function index(LoteRepository $loteRepository, EventoRepository $eventoRepository, RequestStack $requestStack): Response
     {
+        $session = $requestStack->getSession();
+        $evento = $eventoRepository->find($session->get('idEvento'));
+        if ($evento === null) {
+            $this->addFlash('warning', 'Nenhum evento selecionado, ou evento selecionado é inválido!');
+            $this->redirectToRoute('app_evento_index',[], Response::HTTP_SEE_OTHER);
+        }
+        $lotes = $loteRepository->findBy(['tickets.evento'=>$evento]);
         return $this->render('lote/index.html.twig', [
-            'lotes' => $loteRepository->findAll(),
+            'lotes' => $lotes,
         ]);
     }
 
     #[Route('/new', name: 'app_lote_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager, EventoRepository $eventoRepository): Response
+    public function new(Request $request, EntityManagerInterface $entityManager, EventoRepository $eventoRepository, RequestStack $requestStack): Response
     {
+
         $lote = new Lote();
         $lote->setDataEmissao(new \DateTime());
         $form = $this->createForm(LoteType::class, $lote);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $evento = $eventoRepository->find('1ee5501d-c7ae-652c-aa83-7d09c4bf49a2');
+            $session = $requestStack->getSession();
+            $evento = $eventoRepository->find($session->get('idEvento'));
+            if ($evento === null) {
+                $this->addFlash('warning', 'Nenhum evento selecionado, ou evento selecionado é inválido!');
+                $this->redirectToRoute('app_evento_index', [], Response::HTTP_SEE_OTHER);
+            }
+
             for ($i=1; $i<=$lote->getQuantidade();$i++){
                 $ticket = new Ticket();
                 $ticket
@@ -48,10 +63,7 @@ class LoteController extends AbstractController
                     ;
                 $lote->addTicket($ticket);
             }
-
             $entityManager->persist($lote);
-
-//            dump($lote);die;
             $entityManager->flush();
 
             return $this->redirectToRoute('app_lote_index', [], Response::HTTP_SEE_OTHER);
@@ -63,12 +75,21 @@ class LoteController extends AbstractController
         ]);
     }
 
+
     #[Route('/{id}', name: 'app_lote_show', methods: ['GET'])]
     public function show(Lote $lote): Response
     {
         return $this->render('lote/show.html.twig', [
             'lote' => $lote,
         ]);
+    }
+
+    #[Route('/{id}/lista', name: 'app_lote_lista', methods: ['GET'])]
+    public function lista(Evento $evento, RequestStack $requestStack ): Response
+    {
+        $session = $requestStack->getSession();
+        $session->set('idEvento',$evento->getId());
+        return $this->redirectToRoute('app_lote_index', [], Response::HTTP_SEE_OTHER);
     }
 
     #[Route('/{id}/edit', name: 'app_lote_edit', methods: ['GET', 'POST'])]
@@ -89,14 +110,5 @@ class LoteController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_lote_delete', methods: ['POST'])]
-    public function delete(Request $request, Lote $lote, EntityManagerInterface $entityManager): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$lote->getId(), $request->request->get('_token'))) {
-            $entityManager->remove($lote);
-            $entityManager->flush();
-        }
 
-        return $this->redirectToRoute('app_lote_index', [], Response::HTTP_SEE_OTHER);
-    }
 }
